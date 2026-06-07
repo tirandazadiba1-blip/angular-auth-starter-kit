e3df94055d1b5bd890a9acb30b5b0312a1e3cb57
Adiba Tirandaz <tirandazadiba1@gmail.com>
2026-06-08 00:31:42 +0530

    feat: setup application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./features/login/pages/login/login.component').then(
+                (m) => m.LoginComponent
+            )
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () =>
+            import('./features/dashboard/pages/dashboard/dashboard.component').then(
+                (m) => m.DashboardComponent
+            )
+    }
+];
